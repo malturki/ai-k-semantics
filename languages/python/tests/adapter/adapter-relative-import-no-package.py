69acb2e7f0a69a16ecdee53efd7b239fcc3b5df6
Musab Alturki <42160792+malturki@users.noreply.github.com>
2026-07-06 02:04:50 +0000

    Cover multi-clause relative import failures

--- languages/python/tests/adapter/adapter-relative-import-no-package.py
+++ languages/python/tests/adapter/adapter-relative-import-no-package.py
@@ -22,6 +22,25 @@ try:
 except ImportError:
     relative_parent = True
 
-result = relative_bare and relative_alias and relative_module_attr and relative_parent
+relative_multi = False
+try:
+    from . import math, keyword as local_keyword
+except ImportError:
+    relative_multi = True
+
+relative_module_multi = False
+try:
+    from .keyword import kwlist as local_kwlist, softkwlist
+except ImportError:
+    relative_module_multi = True
+
+result = (
+    relative_bare
+    and relative_alias
+    and relative_module_attr
+    and relative_parent
+    and relative_multi
+    and relative_module_multi
+)
 assert result
 result
